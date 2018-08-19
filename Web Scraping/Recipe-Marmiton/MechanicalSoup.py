288d55a874fabf64e78f1c772556d8d4c29d09c8
Tristan Ganry <tristanganry@gmail.com>
2018-08-19 14:05:16 -0400

    Create MechanicalSoup.py

---
+++ Web Scraping/Recipe-Marmiton/MechanicalSoup.py
@@ -0,0 +1,30 @@
+# Using MechanicalSoup to scrape one page of recipe for one ingredient on the best French Website Marmiton
+
+import mechanicalsoup
+
+# Connect to marmiton
+browser = mechanicalsoup.StatefulBrowser()
+ingredient = 'oignon'
+startpage = 1
+myurl = 'https://www.marmiton.org/recettes/recherche.aspx?aqt='+ingredient+'&start='+str(startpage)
+browser.open(myurl)
+
+# Response = 200 => OK
+
+# Get recipes from the page (Title + Url Link)
+tt = []
+ur = []
+for link in browser.get_current_page().select('a.recipe-card'):
+    h4 = link.select('h4')
+    mytitle = h4[0].text.replace('<br />','')
+    myurl = 'https://www.marmiton.org/'+link.attrs['href']
+    tt.append(mytitle)
+    ur.append(myurl)
+    
+# Create and Save csv
+import numpy as np
+import pandas as pd
+recipe = pd.DataFrame(np.column_stack([tt, ur]), 
+                               columns=['Title', 'Url'])
+                               
+recipe.to_csv('recipe.csv', index = False)
